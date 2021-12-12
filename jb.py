5895efdc6669727794b72961ec8ec3b24b86d667
andyo <javabudd@gmail.com>
2021-12-12 14:29:32 -0700

    initial ml commit

---
+++ jb.py
@@ -0,0 +1,134 @@
+import gc
+import json
+
+import optuna
+import pandas as pd
+from lightgbm import LGBMRegressor
+from numerapi import NumerAPI
+
+from utils import (
+    neutralize,
+    get_biggest_change_features,
+    validation_metrics,
+    ERA_COL,
+    DATA_TYPE_COL,
+    TARGET_COLUMNS
+)
+
+TARGET_COL = TARGET_COLUMNS['target']
+
+napi = NumerAPI()
+
+current_round = napi.get_current_round(tournament=8)  # tournament 8 is the primary Numerai Tournament
+
+# Tournament data changes every week so we specify the round in their name. Training
+# and validation data only change periodically, so no need to download them every time.
+print('Downloading dataset files...')
+napi.download_dataset("numerai_training_data.parquet", "training_data.parquet")
+napi.download_dataset("numerai_validation_data.parquet", f"validation_data.parquet")
+napi.download_dataset("features.json", "features.json")
+
+print('Reading minimal training data')
+# read the feature metadata amd get the "small" feature set
+with open("features.json", "r") as f:
+    feature_metadata = json.load(f)
+features = feature_metadata["feature_sets"]["small"]
+# read in just those features along with era and target columns
+read_columns = features + [ERA_COL, DATA_TYPE_COL, TARGET_COL]
+training_data = pd.read_parquet('training_data.parquet', columns=read_columns)
+
+# pare down the number of eras to every 4th era
+# every_4th_era = training_data[ERA_COL].unique()[::4]
+# training_data = training_data[training_data[ERA_COL].isin(every_4th_era)]
+
+# getting the per era correlation of each feature vs the target
+all_feature_corrs = training_data.groupby(ERA_COL).apply(
+    lambda era: era[features].corrwith(era[TARGET_COL])
+)
+
+# find the riskiest features by comparing their correlation vs
+# the target in each half of training data; we'll use these later
+riskiest_features = get_biggest_change_features(all_feature_corrs, 50)
+
+# "garbage collection" (gc) gets rid of unused data and frees up memory
+gc.collect()
+
+model_name = f"model_target"
+
+
+def objective(optuna_trial: optuna.trial):
+    max_depth = optuna_trial.suggest_int("max_depth", 4, 6, log=True)
+
+    params = {
+        "boosting_type": "goss",
+        "n_estimators": optuna_trial.suggest_int("n_estimators", 1800, 2000, log=True),
+        "learning_rate": optuna_trial.suggest_float("learning_rate", .01, .1, log=True),
+        "max_depth": max_depth,
+        "num_leaves": optuna_trial.suggest_int("num_leaves", 4, 2 ^ max_depth, log=True),
+        "feature_fraction": optuna_trial.suggest_float("feature_fraction ", .001, 1, log=True),
+        "bagging_fraction": optuna_trial.suggest_float("bagging_fraction  ", .001, 1, log=True),
+        "min_data_in_leaf": optuna_trial.suggest_int("min_data_in_leaf", 100, 9999, log=True)
+    }
+
+    model = LGBMRegressor(**params)
+
+    # train on all of train and save the model so we don't have to train next time
+    model.fit(
+        training_data.filter(like='feature_', axis='columns'),
+        training_data[TARGET_COL]
+    )
+
+    gc.collect()
+
+    validation_data = pd.read_parquet('validation_data.parquet', columns=read_columns)
+
+    # double check the feature that the model expects vs what is available to prevent our
+    # pipeline from failing if Numerai adds more data and we don't have time to retrain!
+    model_expected_features = model.booster_.feature_name()
+    if set(model_expected_features) != set(features):
+        print(f"New features are available! Might want to retrain model {model_name}.")
+    validation_data.loc[:, f"preds_{model_name}"] = model.predict(
+        validation_data.loc[:, model_expected_features])
+
+    gc.collect()
+
+    # neutralize our predictions to the riskiest features
+    validation_data[f"preds_{model_name}_neutral_riskiest_50"] = neutralize(
+        df=validation_data,
+        columns=[f"preds_{model_name}"],
+        neutralizers=riskiest_features,
+        proportion=1.0,
+        normalize=True,
+        era_col=ERA_COL
+    )
+
+    model_to_submit = f"preds_{model_name}_neutral_riskiest_50"
+
+    # rename best model to "prediction" and rank from 0 to 1 to meet upload requirements
+    validation_data["prediction"] = validation_data[model_to_submit].rank(pct=True)
+
+    validation_stats = validation_metrics(
+        validation_data,
+        [model_to_submit],
+        TARGET_COL
+    )
+
+    print(validation_stats["sharpe"].iloc[-1])
+
+    return validation_stats["sharpe"].iloc[-1]
+
+
+if __name__ == "__main__":
+    study = optuna.create_study(direction="maximize")
+    study.optimize(objective, n_trials=100)
+
+    print("Number of finished trials: {}".format(len(study.trials)))
+
+    print("Best trial:")
+    trial = study.best_trial
+
+    print("  Value: {}".format(trial.value))
+
+    print("  Params: ")
+    for key, value in trial.params.items():
+        print("    {}: {}".format(key, value))
---
+++ utils.py
@@ -0,0 +1,439 @@
+import json
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+import scipy
+from halo import Halo
+from scipy.stats import skew
+
+ERA_COL = "era"
+
+TARGET_COLUMNS = {
+    'target': 'target',
+    'nomi20': 'target_nomi_20',
+    'nomi60': 'target_nomi_60',
+    'jerome20': 'target_jerome_20',
+    'jerome60': 'target_jerome_60',
+    'janet20': 'target_janet_20',
+    'janet60': 'target_janet_60',
+    'ben20': 'target_ben_20',
+    'ben60': 'target_ben_60',
+    'alan20': 'target_alan_20',
+    'alan60': 'target_alan_60',
+    'paul20': 'target_paul_20',
+    'paul60': 'target_paul_60',
+    'george20': 'target_george_20',
+    'george60': 'target_george_60',
+    'william20': 'target_william_20',
+    'william60': 'target_william_60',
+    'arthur20': 'target_arthur_20',
+    'arthur60': 'target_arthur_60',
+    'thomas20': 'target_thomas_20',
+    'thomas60': 'target_thomas_60',
+}
+
+TARGET_OPTIMUM_PARAMS = {
+    TARGET_COLUMNS['target']: {
+        "boosting_type": "goss",
+        "n_estimators": 1854,
+        "learning_rate": 0.020430332790521113,
+        "num_leaves": 7,
+        "max_depth": 5,
+        "feature_fraction": 0.09706028770241243,
+        "bagging_fraction": 0.014553134875531988,
+        "min_data_in_leaf": 142
+    },
+    TARGET_COLUMNS['nomi20']: {
+        "boosting_type": "goss",
+        "n_estimators": 1978,
+        "learning_rate": 0.04378455468413633,
+        "num_leaves": 5,
+        "max_depth": 5,
+        "feature_fraction": 0.11101377120763281,
+        "bagging_fraction": 0.15678624159723772,
+        "min_data_in_leaf": 1201
+    },
+    TARGET_COLUMNS['nomi60']: {
+        "boosting_type": "goss",
+        "n_estimators": 1978,
+        "learning_rate": 0.04378455468413633,
+        "num_leaves": 5,
+        "max_depth": 5,
+        "feature_fraction": 0.11101377120763281,
+        "bagging_fraction": 0.15678624159723772,
+        "min_data_in_leaf": 1201
+    },
+    TARGET_COLUMNS['jerome20']: {
+        "boosting_type": "goss",
+        "n_estimators": 1978,
+        "learning_rate": 0.04378455468413633,
+        "num_leaves": 5,
+        "max_depth": 5,
+        "feature_fraction": 0.11101377120763281,
+        "bagging_fraction": 0.15678624159723772,
+        "min_data_in_leaf": 1201
+    },
+    TARGET_COLUMNS['jerome60']: {
+        "boosting_type": "goss",
+        "n_estimators": 1978,
+        "learning_rate": 0.04378455468413633,
+        "num_leaves": 5,
+        "max_depth": 5,
+        "feature_fraction": 0.11101377120763281,
+        "bagging_fraction": 0.15678624159723772,
+        "min_data_in_leaf": 1201
+    },
+    TARGET_COLUMNS['janet20']: {
+        "boosting_type": "goss",
+        "n_estimators": 1978,
+        "learning_rate": 0.04378455468413633,
+        "num_leaves": 5,
+        "max_depth": 5,
+        "feature_fraction": 0.11101377120763281,
+        "bagging_fraction": 0.15678624159723772,
+        "min_data_in_leaf": 1201
+    },
+    TARGET_COLUMNS['janet60']: {
+        "boosting_type": "goss",
+        "n_estimators": 1842,
+        "learning_rate": 0.026520287506337205,
+        "num_leaves": 6,
+        "max_depth": 5,
+        "feature_fraction": 0.10866666339142045,
+        "bagging_fraction": 0.0178426564762887,
+        "min_data_in_leaf": 270
+    },
+    TARGET_COLUMNS['ben20']: {
+        "boosting_type": "goss",
+        "n_estimators": 1978,
+        "learning_rate": 0.04378455468413633,
+        "num_leaves": 5,
+        "max_depth": 5,
+        "feature_fraction": 0.11101377120763281,
+        "bagging_fraction": 0.15678624159723772,
+        "min_data_in_leaf": 1201
+    },
+    TARGET_COLUMNS['ben60']: {
+        "boosting_type": "goss",
+        "n_estimators": 1978,
+        "learning_rate": 0.04378455468413633,
+        "num_leaves": 5,
+        "max_depth": 5,
+        "feature_fraction": 0.11101377120763281,
+        "bagging_fraction": 0.15678624159723772,
+        "min_data_in_leaf": 1201
+    },
+    TARGET_COLUMNS['alan20']: {
+        "boosting_type": "goss",
+        "n_estimators": 1978,
+        "learning_rate": 0.04378455468413633,
+        "num_leaves": 5,
+        "max_depth": 5,
+        "feature_fraction": 0.11101377120763281,
+        "bagging_fraction": 0.15678624159723772,
+        "min_data_in_leaf": 1201
+    },
+    TARGET_COLUMNS['alan60']: {
+        "boosting_type": "goss",
+        "n_estimators": 1978,
+        "learning_rate": 0.04378455468413633,
+        "num_leaves": 5,
+        "max_depth": 5,
+        "feature_fraction": 0.11101377120763281,
+        "bagging_fraction": 0.15678624159723772,
+        "min_data_in_leaf": 1201
+    },
+    TARGET_COLUMNS['paul20']: {
+        "boosting_type": "goss",
+        "n_estimators": 1978,
+        "learning_rate": 0.04378455468413633,
+        "num_leaves": 5,
+        "max_depth": 5,
+        "feature_fraction": 0.11101377120763281,
+        "bagging_fraction": 0.15678624159723772,
+        "min_data_in_leaf": 1201
+    },
+    TARGET_COLUMNS['paul60']: {
+        "boosting_type": "goss",
+        "n_estimators": 1978,
+        "learning_rate": 0.04378455468413633,
+        "num_leaves": 5,
+        "max_depth": 5,
+        "feature_fraction": 0.11101377120763281,
+        "bagging_fraction": 0.15678624159723772,
+        "min_data_in_leaf": 1201
+    },
+    TARGET_COLUMNS['george20']: {
+        "boosting_type": "goss",
+        "n_estimators": 1978,
+        "learning_rate": 0.04378455468413633,
+        "num_leaves": 5,
+        "max_depth": 5,
+        "feature_fraction": 0.11101377120763281,
+        "bagging_fraction": 0.15678624159723772,
+        "min_data_in_leaf": 1201
+    },
+    TARGET_COLUMNS['george60']: {
+        "boosting_type": "goss",
+        "n_estimators": 1978,
+        "learning_rate": 0.04378455468413633,
+        "num_leaves": 5,
+        "max_depth": 5,
+        "feature_fraction": 0.11101377120763281,
+        "bagging_fraction": 0.15678624159723772,
+        "min_data_in_leaf": 1201
+    },
+    TARGET_COLUMNS['william20']: {
+        "boosting_type": "goss",
+        "n_estimators": 1978,
+        "learning_rate": 0.04378455468413633,
+        "num_leaves": 5,
+        "max_depth": 5,
+        "feature_fraction": 0.11101377120763281,
+        "bagging_fraction": 0.15678624159723772,
+        "min_data_in_leaf": 1201
+    },
+    TARGET_COLUMNS['william60']: {
+        "boosting_type": "goss",
+        "n_estimators": 1978,
+        "learning_rate": 0.04378455468413633,
+        "num_leaves": 5,
+        "max_depth": 5,
+        "feature_fraction": 0.11101377120763281,
+        "bagging_fraction": 0.15678624159723772,
+        "min_data_in_leaf": 1201
+    },
+    TARGET_COLUMNS['arthur60']: {
+        "boosting_type": "goss",
+        "n_estimators": 1978,
+        "learning_rate": 0.04378455468413633,
+        "num_leaves": 5,
+        "max_depth": 5,
+        "feature_fraction": 0.11101377120763281,
+        "bagging_fraction": 0.15678624159723772,
+        "min_data_in_leaf": 1201
+    },
+    TARGET_COLUMNS['arthur60']: {
+        "boosting_type": "goss",
+        "n_estimators": 1978,
+        "learning_rate": 0.04378455468413633,
+        "num_leaves": 5,
+        "max_depth": 5,
+        "feature_fraction": 0.11101377120763281,
+        "bagging_fraction": 0.15678624159723772,
+        "min_data_in_leaf": 1201
+    },
+    TARGET_COLUMNS['thomas20']: {
+        "boosting_type": "goss",
+        "n_estimators": 1978,
+        "learning_rate": 0.04378455468413633,
+        "num_leaves": 5,
+        "max_depth": 5,
+        "feature_fraction": 0.11101377120763281,
+        "bagging_fraction": 0.15678624159723772,
+        "min_data_in_leaf": 1201
+    },
+    TARGET_COLUMNS['thomas60']: {
+        "boosting_type": "goss",
+        "n_estimators": 1958,
+        "learning_rate": 0.0905131449427027,
+        "num_leaves": 7,
+        "max_depth": 5,
+        "feature_fraction": 0.09854767856610315,
+        "bagging_fraction": 0.06908974187675336,
+        "min_data_in_leaf": 154
+    },
+}
+
+DATA_TYPE_COL = "data_type"
+
+spinner = Halo(text='', spinner='dots')
+
+MODEL_FOLDER = "models"
+MODEL_CONFIGS_FOLDER = "model_configs"
+PREDICTION_FILES_FOLDER = "prediction_files"
+
+
+def save_model(model, name):
+    try:
+        Path(MODEL_FOLDER).mkdir(exist_ok=True, parents=True)
+    except Exception as ex:
+        pass
+    pd.to_pickle(model, f"{MODEL_FOLDER}/{name}.pkl")
+
+
+def load_model(name):
+    path = Path(f"{MODEL_FOLDER}/{name}.pkl")
+    if path.is_file():
+        model = pd.read_pickle(f"{MODEL_FOLDER}/{name}.pkl")
+    else:
+        model = False
+    return model
+
+
+def save_model_config(model_config, model_name):
+    try:
+        Path(MODEL_CONFIGS_FOLDER).mkdir(exist_ok=True, parents=True)
+    except Exception as ex:
+        pass
+    with open(f"{MODEL_CONFIGS_FOLDER}/{model_name}.json", 'w') as fp:
+        json.dump(model_config, fp)
+
+
+def load_model_config(model_name):
+    path_str = f"{MODEL_CONFIGS_FOLDER}/{model_name}.json"
+    path = Path(path_str)
+    if path.is_file():
+        with open(path_str, 'r') as fp:
+            model_config = json.load(fp)
+    else:
+        model_config = False
+    return model_config
+
+
+def get_biggest_change_features(corrs, n):
+    all_eras = corrs.index.sort_values()
+    h1_eras = all_eras[:len(all_eras) // 2]
+    h2_eras = all_eras[len(all_eras) // 2:]
+
+    h1_corr_means = corrs.loc[h1_eras, :].mean()
+    h2_corr_means = corrs.loc[h2_eras, :].mean()
+
+    corr_diffs = h2_corr_means - h1_corr_means
+    worst_n = corr_diffs.abs().sort_values(ascending=False).head(n).index.tolist()
+    return worst_n
+
+
+def get_time_series_cross_val_splits(data, cv=3, embargo=12):
+    all_train_eras = data[ERA_COL].unique()
+    len_split = len(all_train_eras) // cv
+    test_splits = [all_train_eras[i * len_split:(i + 1) * len_split] for i in range(cv)]
+    # fix the last test split to have all the last eras, in case the number of eras wasn't divisible by cv
+    test_splits[-1] = np.append(test_splits[-1], all_train_eras[-1])
+
+    train_splits = []
+    for test_split in test_splits:
+        test_split_max = int(np.max(test_split))
+        test_split_min = int(np.min(test_split))
+        # get all of the eras that aren't in the test split
+        train_split_not_embargoed = [e for e in all_train_eras if not (test_split_min <= int(e) <= test_split_max)]
+        # embargo the train split so we have no leakage.
+        # one era is length 5, so we need to embargo by target_length/5 eras.
+        # To be consistent for all targets, let's embargo everything by 60/5 == 12 eras.
+        train_split = [e for e in train_split_not_embargoed if
+                       abs(int(e) - test_split_max) > embargo and abs(int(e) - test_split_min) > embargo]
+        train_splits.append(train_split)
+
+    # convenient way to iterate over train and test splits
+    train_test_zip = zip(train_splits, test_splits)
+    return train_test_zip
+
+
+def neutralize(df,
+               columns,
+               neutralizers=None,
+               proportion=1.0,
+               normalize=True,
+               era_col="era"):
+    if neutralizers is None:
+        neutralizers = []
+    unique_eras = df[era_col].unique()
+    computed = []
+    for u in unique_eras:
+        df_era = df[df[era_col] == u]
+        scores = df_era[columns].values
+        if normalize:
+            scores2 = []
+            for x in scores.T:
+                x = (scipy.stats.rankdata(x, method='ordinal') - .5) / len(x)
+                x = scipy.stats.norm.ppf(x)
+                scores2.append(x)
+            scores = np.array(scores2).T
+        exposures = df_era[neutralizers].values
+
+        scores -= proportion * exposures.dot(
+            np.linalg.pinv(exposures.astype(np.float32)).dot(scores.astype(np.float32)))
+
+        scores /= scores.std(ddof=0)
+
+        computed.append(scores)
+
+    return pd.DataFrame(np.concatenate(computed),
+                        columns=columns,
+                        index=df.index)
+
+
+def neutralize_series(series, by, proportion=1.0):
+    scores = series.values.reshape(-1, 1)
+    exposures = by.values.reshape(-1, 1)
+
+    # this line makes series neutral to a constant column so that it's centered and for sure gets corr 0 with exposures
+    exposures = np.hstack(
+        (exposures,
+         np.array([np.mean(series)] * len(exposures)).reshape(-1, 1)))
+
+    correction = proportion * (exposures.dot(
+        np.linalg.lstsq(exposures, scores, rcond=None)[0]))
+    corrected_scores = scores - correction
+    neutralized = pd.Series(corrected_scores.ravel(), index=series.index)
+    return neutralized
+
+
+def unif(df):
+    x = (df.rank(method="first") - 0.5) / len(df)
+    return pd.Series(x, index=df.index)
+
+
+def get_feature_neutral_mean(df, prediction_col, target_col):
+    feature_cols = [c for c in df.columns if c.startswith("feature")]
+    df.loc[:, "neutral_sub"] = neutralize(df, [prediction_col],
+                                          feature_cols)[prediction_col]
+    scores = df.groupby("era").apply(
+        lambda x: (unif(x["neutral_sub"]).corr(x[target_col]))).mean()
+    return np.mean(scores)
+
+
+def fast_score_by_date(df, columns, target, tb=None, era_col="era"):
+    unique_eras = df[era_col].unique()
+    computed = []
+    for u in unique_eras:
+        df_era = df[df[era_col] == u]
+        era_pred = np.float64(df_era[columns].values.T)
+        era_target = np.float64(df_era[target].values.T)
+
+        if tb is None:
+            ccs = np.corrcoef(era_target, era_pred)[0, 1:]
+        else:
+            tbidx = np.argsort(era_pred, axis=1)
+            tbidx = np.concatenate([tbidx[:, :tb], tbidx[:, -tb:]], axis=1)
+            ccs = [np.corrcoef(era_target[tmpidx], tmppred[tmpidx])[0, 1] for tmpidx, tmppred in zip(tbidx, era_pred)]
+            ccs = np.array(ccs)
+
+        computed.append(ccs)
+
+    return pd.DataFrame(np.array(computed), columns=columns, index=df[era_col].unique())
+
+
+def validation_metrics(validation_data, pred_cols, target_col):
+    validation_stats = pd.DataFrame()
+    for pred_col in pred_cols:
+        validation_correlations = validation_data.groupby(ERA_COL).apply(
+            lambda d: unif(d[pred_col]).corr(d[target_col]))
+
+        mean = validation_correlations.mean()
+        std = validation_correlations.std(ddof=0)
+        sharpe = mean / std
+
+        validation_stats.loc["mean", pred_col] = mean
+        validation_stats.loc["std", pred_col] = std
+        validation_stats.loc["sharpe", pred_col] = sharpe
+
+    # .transpose so that stats are columns and the model_name is the row
+    return validation_stats.transpose()
+
+
+def download_data(napi, filename, dest_path):
+    spinner.start(f'Downloading {dest_path}')
+    napi.download_dataset(filename, dest_path)
+    spinner.succeed()
